633f9f1bb09f5c56b40cea4795f34d20e75c3738
Helen Raudsepp <helen.raudsepp@k-print.ee>
2022-03-02 20:39:27 +0200

    add new projects

---
+++ DiceGame/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace DiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //mängus osaleb 2 mängijat - arvuti (cpu) ja kasutaja (user)
+            //mõlemad mängijad viskavad täringuid;
+            //programm kontrollib, kumb mängija viskas rohkem;
+            //mängija, kes viskab rohkem, on mängu võitja
+            //*täringuid visatakse 3 korda
+            //programm kuulutab võitja
+
+            Random rnd = new Random();
+                                   
+            int cpuScore = 0;
+            int userScore = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+
+                int cpuRandom = rnd.Next(1, 7);
+                int userRandom = rnd.Next(1, 7);
+
+                Console.WriteLine($"Arvuti viskas {cpuRandom}. Kasutaja viskas {userRandom}.");
+                if (cpuRandom < userRandom)
+                {
+
+                    Console.WriteLine($"Kasutaja on mängu võitnud. Kasutaja punktide arv on {userScore + 1}.");
+                    userScore = userScore + 1;
+
+                }
+
+                else if (cpuRandom > userRandom)
+                {
+                    Console.WriteLine($"Arvuti on mängu võitnud. Arvuti puntide arv on {cpuScore + 1}.");
+                    cpuScore = cpuScore + 1;
+                }
+
+                else
+                {
+                    Console.WriteLine("Viik!");
+                }
+
+                
+            }
+            Console.WriteLine("Suurima punktidega arvuga mängija on üldvõitja! Viigi korral proovige uuesti!");
+        }
+    }
+}
---
+++ LoginValidation/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace LoginValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kastajal sisestada kasutajatunnus ja salasõna;
+            //kui sisestatud kasutajatunnus on "admin" ja sisestatud salasõna on "admind1234";
+            //siis konsoolis kuvatakse "Tere tulemast!";
+            //muul juhul konsoolis kuvatakse "Vale kasutajatunnus või salasõna. Proovi uuesti.";
+            //kasutajal on kolm katset
+                       
+            int i = 0;
+
+            while (i < 3)
+            {
+                Console.WriteLine("Sisesta kastajatunnus:");
+                string userName = Console.ReadLine();
+                Console.WriteLine("Sisesta salasõna:");
+                string userPassword = Console.ReadLine();
+
+                if (userName == "admin" && userPassword == "admin1234")
+                {
+                    Console.WriteLine("Tere tulemast!");
+                    break;
+
+                }
+
+                else
+                {
+                    i++;
+                    Console.WriteLine($"Vale kasutajatunnus või parool, proovi uuesti. Katseid jäänud {3 - (i)}.");
+                }
+            }
+
+        }
+    }
+}
